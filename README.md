9324037e4d094dd8bb8078e188ec17c9825032c6
allez_ryan_venga <120065584+MiAmigoRyan@users.noreply.github.com>
2023-03-25 12:57:13 -0600

    minor refractoring and README.md update

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
 # FoodTruckProject
 
 # Overview
+<br> Users will input and rate a list of food trucks, then view the average rating and the highest-rated truck.
 
 # Technologies Used
 <br>Java
 <br>OOP
+<br>Slack
+<br>Zoom
+<br>Stack Overflow
+<br>Eclipse
 
 # Lessons Learned
+<br>Wow, where to start!?
+<br>  This started out fairly difficult, then felt simple. Near the end however, I felt that there were some concepts I didn't grasp.
+<br> I feel comfortable walking through small steps in plain english with a TA. During this project I often found myself over-complicating the code in a 'shotgun' approach to solve an error.
+<br> I failed to commit earlier in my project and wish I had remembered to do so. It would have been nice to see some of the steps I solved.
+<br> I can boil down the 3 biggest take away's.
+<br>1 . Methods are really helpful in organizing the steps.
+<br>2 . While loops and booleans went hand in hand for this particular program.
+<br>3 . When the code get's frustrating it's helpful to try to simplify what you are trying to do.
--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -6,22 +6,22 @@ import com.skilldistillery.foodtruck.entities.FoodTruck;
 
 public class FoodTruckApp {
 
-	public Scanner sc = new Scanner(System.in);
+	private Scanner sc = new Scanner(System.in);
+	private FoodTruck[] fleet = new FoodTruck[5];
 
 	public static void main(String[] args) {
 		FoodTruckApp fta = new FoodTruckApp();
 
-		FoodTruck[] fleet = new FoodTruck[5];
-
-		fta.run(fleet);
+		fta.run();
 	}
 
-	public void run(FoodTruck[] fleet) {
+	public void run() {
 		System.out.println("Get ready to Rate some Food Trucks!");
 
-		boolean quit = false;
+		boolean running = true;
 		int counter = 0;
-		while (quit == false && counter < 5) {
+		
+		while (running == true && counter < 5) {
 			for (int truck = 0; truck < fleet.length; truck++) {
 				int truckId = truck + 1;
 				System.out.println("\nEnter Food Truck number " + truckId);
@@ -29,10 +29,9 @@ public class FoodTruckApp {
 				System.out.println("What is the Name of the Food Truck?");
 				String name = sc.next();
 				if (name.equals("quit")) {
-					quit = true;
+					running = false;
 					break;
 				}
-
 				System.out.println("What type of food does this truck serve? ");
 				String type = sc.next();
 
@@ -43,52 +42,50 @@ public class FoodTruckApp {
 					System.out.println("not a valid rating you must enter number between 1 and 3");
 					rating = sc.nextInt();
 				}
-
 				fleet[truck] = new FoodTruck(truckId, name, type, rating);
 
-				// System.out.println(fleet[truck].toString());
 				counter++;
 			}
 		}
-		if (quit == true || counter >= 5) {
-			menu(fleet);
+		if (running == false || counter >= 5) {
+			menu();
 		}
 		sc.close();
 	}
-
 	public void exit() {
 		System.out.println("Thank you for rating Food Trucks!!!");
 
 	}
-
-	public void menu(FoodTruck[] fleet) {
-		while (true) {
-			System.out.println(" *********************************************" + "\n 1. List Food Trucks"
-					+ "\n 2. View Average Rating of Food Trucks" + "\n 3. View Highest Rated Food Truck" + "\n 4. Quit "
-					+ "\n *********************************************");
+	public void menu() {
+		boolean running = true;
+		while (running) {
+			System.out.println(   " *********************************************" 
+								+ "\n 1. List Food Trucks"
+								+ "\n 2. View Average Rating of Food Trucks" 
+								+ "\n 3. View Highest Rated Food Truck" 
+								+ "\n 4. Quit "
+								+ "\n *********************************************");
 			int choice = sc.nextInt();
 			switch (choice) {
 			case 1:
-				// LIST
-				System.out.println(truckNames(fleet));
+				System.out.println(truckNames());
 				break;
 			case 2:
-				// AVERAGE RATING
-				System.out.println(avgRating(fleet));
+				System.out.println(avgRating());
 				break;
 			case 3:
-				System.out.println(topRating(fleet));
+				System.out.println(topRating());
 				break;
 			case 4:
 				exit();
+				running = false;
 				break;
 			default:
 				System.out.println("not a valid entry.");
 			}
 		}
 	}
-
-	public String truckNames(FoodTruck[] fleet) {
+	public String truckNames() {
 		String names = "";
 		for (int truck = 0; truck < fleet.length; truck++) {
 			if (fleet[truck] != null) {
@@ -97,8 +94,7 @@ public class FoodTruckApp {
 		}
 		return names;
 	}
-
-	public double avgRating(FoodTruck[] fleet) {
+	public double avgRating() {
 		double totalRating = 0.0;
 		int numTrucks = 0;
 		for (int truck = 0; truck < fleet.length; truck++) {
@@ -109,8 +105,7 @@ public class FoodTruckApp {
 		}
 		return totalRating / numTrucks;
 	}
-
-	public FoodTruck topRating(FoodTruck[] fleet) {
+	public FoodTruck topRating() {
 		FoodTruck topRated = null;
 		int nextRate = 0;
 		for (int truck = 0; truck < fleet.length; truck++) {

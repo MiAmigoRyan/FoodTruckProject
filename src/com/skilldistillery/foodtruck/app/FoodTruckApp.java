1a999ecc38ede2ac3bf70d73abd201f54430bd83
allez_ryan_venga <120065584+MiAmigoRyan@users.noreply.github.com>
2023-03-25 12:09:04 -0600

    realized I hadn't made any commits since the initalgit add .! many updates and changes

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,19 +1,122 @@
 package com.skilldistillery.foodtruck.app;
 
+import java.util.Scanner;
+
+import com.skilldistillery.foodtruck.entities.FoodTruck;
+
 public class FoodTruckApp {
 
-	public static void main(String[] args) {
-		 
-		run();
+	public Scanner sc = new Scanner(System.in);
 
+	public static void main(String[] args) {
+		FoodTruckApp fta = new FoodTruckApp();
+
+		FoodTruck[] fleet = new FoodTruck[5];
+
+		fta.run(fleet);
 	}
-	public run() {
-	while() {
-		
-	}if( =="quit") {
-		System.out.println("goodbye");
-		
+
+	public void run(FoodTruck[] fleet) {
+		System.out.println("Get ready to Rate some Food Trucks!");
+
+		boolean quit = false;
+		int counter = 0;
+		while (quit == false && counter < 5) {
+			for (int truck = 0; truck < fleet.length; truck++) {
+				int truckId = truck + 1;
+				System.out.println("\nEnter Food Truck number " + truckId);
+
+				System.out.println("What is the Name of the Food Truck?");
+				String name = sc.next();
+				if (name.equals("quit")) {
+					quit = true;
+					break;
+				}
+
+				System.out.println("What type of food does this truck serve? ");
+				String type = sc.next();
+
+				System.out.println("On a scale of 1-3 (3 being the best) " + "what rating would you give this truck? ");
+				int rating = sc.nextInt();
+
+				while (rating > 3 || rating < 1) {
+					System.out.println("not a valid rating you must enter number between 1 and 3");
+					rating = sc.nextInt();
+				}
+
+				fleet[truck] = new FoodTruck(truckId, name, type, rating);
+
+				// System.out.println(fleet[truck].toString());
+				counter++;
+			}
+		}
+		if (quit == true || counter >= 5) {
+			menu(fleet);
+		}
 	}
+
+	public void exit() {
+		System.out.println("Thank you for rating Food Trucks!!!");
+
+		sc.close();
+	}
+
+	public void menu(FoodTruck[] fleet) {
+
+		System.out.println(" *********************************************" + "\n 1. List Food Trucks"
+				+ "\n 2. View Average Rating of Food Trucks" + "\n 3. View Highest Rated Food Truck" + "\n 4. Quit "
+				+ "\n *********************************************");
+		int choice = sc.nextInt();
+		switch (choice) {
+		case 1:
+			// LIST
+			System.out.println(truckNames(fleet));
+			break;
+		case 2:
+			// AVERAGE RATING
+			System.out.println(avgRating(fleet));
+			break;
+		case 3:
+			System.out.println(topRating(fleet));
+			break;
+		case 4:
+			exit();
+			break;
+		default:
+
+		}
+	}
+
+	public String truckNames(FoodTruck[] fleet) {
+		String names = "";
+		for (int truck = 0; truck < fleet.length; truck++) {
+			if (fleet[truck] != null) {
+				names += fleet[truck].getName() + "\n" + "\t";
+			}
+		}
+		return names;
+	}
+
+	public double avgRating(FoodTruck[] fleet) {
+		double totalRating = 0.0;
+		int numTrucks = 0;
+		for (int truck = 0; truck < fleet.length; truck++) {
+			if (fleet[truck] != null) {
+				totalRating += fleet[truck].getRating();
+				numTrucks++;
+			}
+		}
+		return totalRating / numTrucks;
+	}
+
+	public FoodTruck topRating(FoodTruck[] fleet) {
+		FoodTruck topRated = null;
+		for (int truck = 0; truck < fleet.length; truck++) {
+			if (fleet[truck] != null && topRated == null || fleet[truck].getRating() > topRated.getRating()) {
+				topRated = fleet[truck];
+			}
+		}
+		return topRated;
 	}
 
 }
--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -1,14 +1,67 @@
 package com.skilldistillery.foodtruck.entities;
 
 public class FoodTruck {
-	private static String truckId;
-	private String rating;
+	private int truckId;
+	private int rating;
 	private String foodType;
-	private String name;
+	private String name;	
+
+
+	//constructors	
+	public FoodTruck (int truckId, String name, String foodType, int rating ) {
+		this.truckId = truckId;
+		this.name = name;
+		this.foodType = foodType;
+		this.rating = rating;
+	}
 	
-	//constructors
-	
-	// getters/ setters
-	
+
 	//toString method
+	@Override
+	public String toString() {
+		return "FoodTruck [truckId=" + truckId + ", rating=" + rating + ", foodType=" + foodType + ", name=" + name
+				+ "]";
+	
+	}
+
+
+	public int getTruckId() {
+		return truckId;
+	}
+
+
+	public void setTruckId(int truckId) {
+		this.truckId = truckId;
+	}
+
+
+	public int getRating() {
+		return rating;
+	}
+
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
